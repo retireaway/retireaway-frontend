12065eefc10b7c937ed782470f62ba39d70c9050
Jesuseun Jeremiah Olatunde <jeremiah.olatunde.dev@proton.me>
2026-01-21 06:23:44 +0100

    chore: configure tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "node:path";
+import { defineConfig } from "vite";
+
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +12,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
