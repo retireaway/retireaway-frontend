985d11299fba17580a255b47ed23b4f51e759ffe
Jesuseun Jeremiah Olatunde <jeremiah.olatunde.dev@proton.me>
2026-01-21 06:56:05 +0100

    feat: add og meta tags

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,14 @@
+import * as Wouter from "wouter";
+import { Home } from "./pages/home";
+
 export function App() {
-  return "RetireAway Application";
+  return (
+    <Wouter.Router base="/">
+      <Wouter.Switch>
+        <Wouter.Route path="/">
+          <Home />
+        </Wouter.Route>
+      </Wouter.Switch>
+    </Wouter.Router>
+  );
 }
---
+++ src/app/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <section className="">
+      <header>
+        <h1>RetireAway</h1>
+      </header>
+    </section>
+  );
+}
