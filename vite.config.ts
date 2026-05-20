8b4083a4bbfb9b21ddc13ef7e2cdd458238857e8
Jesuseun Jeremiah Olatunde <jeremiah.olatunde.dev@proton.me>
2026-05-20 02:17:33 +0100

    chore: vite https setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,22 @@
 import path from "node:path";
-import { defineConfig } from "vite";
 
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     host: "0.0.0.0",
+    port: 5174,
+    strictPort: true,
   },
+  plugins: [svgr(), react(), tailwindcss(), basicSsl()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [
-    tailwindcss(),
-    svgr(),
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
 });
